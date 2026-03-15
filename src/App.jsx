b7c256732f451fef9603bcf79f801d5d8c4fae31
Yuteka <yutekahema003@gmail.com>
2026-03-15 18:27:39 +0530

    fix import casing to match Pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import ProductDetail from './pages/ProductDetail';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import ForgotPassword from './pages/ForgotPassword';
-import Cart from './pages/Cart';
-import Payment from './pages/Payment';
-import Orders from './pages/Orders';
-import OrderTrack from './pages/OrderTrack';
+import Home from './Pages/Home';
+import ProductDetail from './Pages/ProductDetail';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import ForgotPassword from './Pages/ForgotPassword';
+import Cart from './Pages/Cart';
+import Payment from './Pages/Payment';
+import Orders from './Pages/Orders';
+import OrderTrack from './Pages/OrderTrack';
 
 export default function App() {
   return (
